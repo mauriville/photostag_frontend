18ba776b43cd85ab7e5a9ad213e166aebbac684f
Mauricio Villegas <maurikiller732@hotmail.com>
2021-10-14 23:41:21 -0400

    images crud added

--- src/views/Image/Image.js
+++ src/views/Image/Image.js
@@ -1,4 +1,6 @@
 import MainService from "@/services/MainService.js";
+import dataTable from "@/components/Datatable";
+window.$ = window.jQuery = require("jquery");
 
 export default {
     name: "Image",
@@ -11,11 +13,172 @@ export default {
                 "url": ss.indexImage(),
                 headers: ss.getToken(),
             },
+            baseURL : process.env.VUE_APP_MAIN_SERVICE,
+            columns: [
+                { data: 'id', name: 'id', orderable: false, searchable: false, visible: false },
+                { data: 'DT_RowIndex', name: 'DT_RowIndex', title: 'N', orderable: false, searchable: false },
+                { data: 'Tittle', name: 'Tittle', title: 'Tittle' },
+                {
+                    data: 'action',
+                    orderable: false,
+                    title: 'Acciones',
+                    searchable: false
+                },
+            ],
+            images: [],
+            image: {},
+            isLoading: false,
+            errorBag: {}
         };
     },
     methods: {
+        loadFile(input) {
+            this.isLoadingFile = true;
+            input = event.target;
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+                reader.readAsDataURL(input.files[0]);
+                var data = new FormData();
+                data.append('File', input.files[0]);
+                this.ss.uploadFile(data)
+                    .then(result => {
+                        if (result.data.success) {
+                            this.$bvToast.toast(
+                                result.data.msg,
+                                {
+                                    title: 'Correcto',
+                                    variant: 'info',
+                                    autoHideDelay: 5000
+                                }
+                            )
+                            this.image.ImageUrl = result.data.data;
+                            this.image.ThumbnailUrl = result.data.data;
+                        } else {
+                            this.$bvToast.toast(
+                                result.data.msg,
+                                {
+                                    title: 'Oops!',
+                                    variant: 'danger',
+                                    autoHideDelay: 5000
+                                }
+                            )
+                        }
+                        this.isLoadingFile = false;
+                    })
+                    .catch(error => {
+                        console.log(error);
+                        this.$bvToast.toast(
+                            'Error subiendo archivo',
+                            {
+                                title: 'Oops!',
+                                variant: 'danger',
+                                autoHideDelay: 5000
+                            }
+                        )
+                        this.isLoadingFile = false;
+                    });
+            }
+        },
+        newImage() {
+            this.image = {};
+            this.$refs['frm-image'].show();
+        },
+        showImage(id) {
+            this.isLoading = true;
+            this.ss.showImage(id).then(
+                (result) => {
+                    let response = result.data;
+                    this.image = response.data;
+                    this.$refs['view-image'].show();
+                    this.isLoading = false;
+                }
+            ).catch(error => {
+                console.log(error);
+                this.isLoading = false;
+            });
+        },
+        editImage() {
+            this.$refs['frm-image'].show();
+            this.$refs['view-image'].hide();
+        },
+        cancelImage() {
+            if (this.image.id) {
+                this.$refs['view-image'].show();
+            }
+            this.$refs['frm-image'].hide();
+        },
+        saveImage() {
+            this.ss.storeImage(this.image).then(
+                (result) => {
+                    console.log(result);
+                    let response = result.data;
+                    this.$bvToast.toast(
+                        response.msg,
+                        {
+                            title: 'Correcto',
+                            variant: 'success',
+                            autoHideDelay: 5000
+                        }
+                    )
+                    //this.$refs['view-consulta'].show(); //para volver al modal
+                    console.log(response);
+                    this.$refs['frm-image'].hide();
+                    this.$refs['datatable-image'].reload();
+                })
+                .catch((error) => {
+                    this.errorBag = error.response.data.errors;
+                    this.$bvToast.toast(
+                        'Problema al Guardar el Registro, favor verificar los Datos',
+                        {
+                            title: 'Error',
+                            variant: 'danger',
+                            autoHideDelay: 5000
+                        }
+                    )
+                });
+        },
+        deleteImage() {
+            this.$swal({
+                title: "Estas seguro que deseas eliminar?",
+                text: "Esta accion es irreversible!",
+                icon: "warning",
+                buttons: true,
+                dangerMode: true,
+            })
+                .then((willDelete) => {
+                    if (willDelete) {
+                        this.ss.destroyImage(this.image)
+                            .then((result) => {
+                                let response = result.data;
+                                console.log(response);
+                                this.$bvToast.toast(
+                                    response.msg,
+                                    {
+                                        title: 'Correcto',
+                                        variant: 'success',
+                                        autoHideDelay: 5000
+                                    }
+                                )
+                                this.$refs['view-image'].hide();
+                                this.$refs['datatable-image'].reload();
+                            })
+                            .catch(error => {
+                                console.log(error);
+                            })
+                    } else {
+                        //swal("Your imaginary file is safe!");
+                    }
+                });
+        },
+        draw() {
+            window.$('.btn-datatable-Image').on('click', (evt) => {
+                const data = window.$(evt.target)[0].id;
+                this.showImage(data);
+            });
+        }
     },
     components: {
+        dataTable
     },
     mounted() {
     }
--- src/views/Image/Image.vue
+++ src/views/Image/Image.vue
@@ -1,12 +1,109 @@
 <template>
     <div>
-        <div style="background-color: #003f8a">
-            <v-card-title>
-                <p class="white--text">Image</p>
-            </v-card-title>
+        <div class="page-header-title">
+            <br />
+            <h4 class="page-title">{{ $t("nav.Image") }}</h4>
         </div>
-        <div class="container-fluid">
+        <div class="page-content-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h6 class="card-title text-dark m-0">
+                                    Detalle de Registros
+                                    <div class="float-right">
+                                        <button
+                                            @click.prevent="newImage"
+                                            class="btn btn-sm btn-success waves-effect waves-light m-l-10"
+                                        >
+                                            <i class="fa fa-plus"></i>
+                                            {{ $t("actions.new") }}
+                                        </button>
+                                    </div>
+                                </h6>
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <data-table
+                                        :columns="columns"
+                                        :ajax="ajax"
+                                        width="100%"
+                                        @draw="draw"
+                                        ref="datatable-image"
+                                    ></data-table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+        <!-- vista de consulta-->
+        <b-modal ref="view-image" size="md" title="Image">
+            <div class="row">
+                <div class="col-md-6">
+                    <img :src="baseURL+'storage/documents/'+image.ImageUrl" style="width: 100%" />
+                </div>
+                <div class="col-md-6">
+                    <h4>Tittle</h4>
+                    <p class="text-muted">{{ image.Tittle }}</p>
+                </div>
+            </div>
+            <template #modal-footer>
+                <a @click.prevent="editImage" class="btn btn-warning">
+                    <i class="fa fa-edit"></i>
+                    {{ $t("actions.edit") }}
+                </a>
+                <a @click.prevent="deleteImage" class="btn btn-danger">
+                    <i class="fa fa-trash"></i>
+                    {{ $t("actions.destroy") }}
+                </a>
+            </template>
+        </b-modal>
+        <!-- vista de consulta-->
+        <b-modal ref="frm-image" size="lg" title="Image">
+            <form role="form">
+                <div class="form-group">
+                    <label for="Tittle">Tittle</label>
+                    <input type="text" class="form-control" name="Tittle" v-model="image.Tittle" />
+                    <ul
+                        class="parsley-errors-list filled"
+                        id="parsley-id-19"
+                        v-if="errorBag.Tittle"
+                    >
+                        <li class="parsley-required">{{ errorBag.Tittle }}</li>
+                    </ul>
+                </div>
+                <div class="form-group">
+                    <label for="email">Fotografia</label>
+                    <b-form-file
+                        @change="loadFile('Fotografia')"
+                        placeholder="Selecciona o suelta un Archivo..."
+                        drop-placeholder="Suelta el Archivo aqui..."
+                    ></b-form-file>
+                    <span v-show="isLoadingFile" class="text-success">
+                        <i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
+                        Cargando Archivo
+                        <span class="sr-only">Cargando...</span>
+                    </span>
+                    <span v-show="!isLoadingFile && image.ImageUrl" class="text-info">
+                        <i class="fa fa-thumbs-o-up"></i> Archivo
+                        Cargado!
+                    </span>
+                </div>
+            </form>
+            <template #modal-footer>
+                <a @click.prevent="saveImage()" class="btn btn-success">
+                    <i class="fa fa-save"></i>
+                    {{ $t("actions.save") }}
+                </a>
+                <button type="button" class="btn btn-danger" @click.prevent="cancelImage()">
+                    <i class="fa fa-window-close"></i>
+                    {{ $t("actions.cancel") }}
+                </button>
+            </template>
+        </b-modal>
     </div>
 </template>
 
