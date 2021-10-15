d6a4371c3ec26814fb54b6b38fe5f51f8efd02ca
Mauricio Villegas <maurikiller732@hotmail.com>
2021-10-14 23:00:33 -0400

    adding services to main service

--- src/services/MainService.js
+++ src/services/MainService.js
@@ -27,5 +27,59 @@ export default class MainService {
         return axios.post('api/uploadFile', item);
     }
 
+    showImage(id) {
+        return axios.get('api/Image/show?id=' + id);
+    }
 
+    indexImage() {
+        return axios.defaults.baseURL + 'api/Image/index';
+    }
+
+    listImage() {
+        return axios.get('api/Image/list');
+    }
+
+    storeImage(item) {
+        return axios.post('api/Image/store', item);
+    }
+
+    destroyImage(item) {
+        return axios.post('api/Image/destroy', item);
+    }
+
+    showTag(id) {
+        return axios.get('api/Tag/show?id=' + id);
+    }
+
+    indexTag() {
+        return axios.defaults.baseURL + 'api/Tag/index';
+    }
+
+    listTag() {
+        return axios.get('api/Tag/list');
+    }
+
+    storeTag(item) {
+        return axios.post('api/Tag/store', item);
+    }
+
+    destroyTag(item) {
+        return axios.post('api/Tag/destroy', item);
+    }
+
+    showTagImage(id) {
+        return axios.get('api/TagImage/show?id=' + id);
+    }
+
+    listTagImage() {
+        return axios.get('api/TagImage/list');
+    }
+
+    storeTagImage(item) {
+        return axios.post('api/TagImage/store', item);
+    }
+
+    destroyTagImage(item) {
+        return axios.post('api/TagImage/destroy', item);
+    }
 }
