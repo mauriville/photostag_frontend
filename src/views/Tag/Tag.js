584570b90f6a6fc31ef1894b31b536bab60a23eb
Mauricio Villegas <maurikiller732@hotmail.com>
2021-10-14 23:20:36 -0400

    crud de tags

--- src/views/Tag/Tag.js
+++ src/views/Tag/Tag.js
@@ -1,4 +1,6 @@
 import MainService from "@/services/MainService.js";
+import dataTable from "@/components/Datatable";
+window.$ = window.jQuery = require("jquery");
 
 export default {
     name: "Tag",
@@ -8,14 +10,127 @@ export default {
             msg: "Tag",
             ss: ss,
             ajax: {
-                "url": ss.indexImage(),
+                "url": ss.indexTag(),
                 headers: ss.getToken(),
             },
+            columns: [
+                { data: 'id', name: 'id', orderable: false, searchable: false, visible: false },
+                { data: 'DT_RowIndex', name: 'DT_RowIndex', title: 'N', orderable: false, searchable: false },
+                { data: 'Label', name: 'Label', title: 'Label' },
+                {
+                    data: 'action',
+                    orderable: false,
+                    title: 'Acciones',
+                    searchable: false
+                },
+            ],
+            tags: [],
+            tag: {},
+            isLoading: false,
+            errorBag: {}
         };
     },
     methods: {
+        newTag() {
+            this.tag = {};
+            this.$refs['frm-tag'].show();
+        },
+        showTag(id) {
+            this.isLoading=true;
+            this.ss.showTag(id).then(
+                (result) => {
+                    let response = result.data;
+                    this.tag = response.data;
+                    this.$refs['view-tag'].show();
+                    this.isLoading=false;
+                }
+            ).catch(error => {
+                console.log(error);
+                this.isLoading=false;
+            });
+        },
+        editTag() {
+            this.$refs['frm-tag'].show();
+            this.$refs['view-tag'].hide();
+        },
+        cancelTag() {
+            if (this.tag.id) {
+                this.$refs['view-tag'].show();
+            }
+            this.$refs['frm-tag'].hide();
+        },
+        saveTag() {
+            this.ss.storeTag(this.tag).then(
+                (result) => {
+                    console.log(result);
+                    let response = result.data;
+                    this.$bvToast.toast(
+                        response.msg,
+                        {
+                            title: 'Correcto',
+                            variant: 'success',
+                            autoHideDelay: 5000
+                        }
+                    )
+                    //this.$refs['view-consulta'].show(); //para volver al modal
+                    console.log(response);
+                    this.$refs['frm-tag'].hide();
+                    this.$refs['datatable-tag'].reload();
+                })
+                .catch((error) => {
+                    this.errorBag = error.response.data.errors;
+                    this.$bvToast.toast(
+                        'Problema al Guardar el Registro, favor verificar los Datos',
+                        {
+                            title: 'Error',
+                            variant: 'danger',
+                            autoHideDelay: 5000
+                        }
+                    )
+                });
+        },
+        deleteTag() {
+            this.$swal({
+                title: "Estas seguro que deseas eliminar?",
+                text: "Esta accion es irreversible!",
+                icon: "warning",
+                buttons: true,
+                dangerMode: true,
+            })
+                .then((willDelete) => {
+                    if (willDelete) {
+                        this.ss.destroyTag(this.tag)
+                            .then((result) => {
+                                let response = result.data;
+                                console.log(response);
+                                this.$bvToast.toast(
+                                    response.msg,
+                                    {
+                                        title: 'Correcto',
+                                        variant: 'success',
+                                        autoHideDelay: 5000
+                                    }
+                                )
+                                this.$refs['view-tag'].hide();
+                                this.$refs['datatable-tag'].reload();
+                            })
+                            .catch(error => {
+                                console.log(error);
+                            })
+                    } else {
+                        //swal("Your imaginary file is safe!");
+                    }
+                });
+        },
+        draw() {
+            window.$('.btn-datatable-Tag').on('click', (evt) => {
+                const data = window.$(evt.target)[0].id;
+                this.showTag(data);
+            });
+        }
     },
     components: {
+        dataTable
     },
     mounted() {
     }
--- src/views/Tag/Tag.vue
+++ src/views/Tag/Tag.vue
@@ -1,12 +1,100 @@
 <template>
     <div>
-        <div style="background-color: #003f8a">
-            <v-card-title>
-                <p class="white--text">Tag</p>
-            </v-card-title>
+        <div class="page-header-title">
+            <br />
+            <h4 class="page-title">{{ $t("nav.Tag") }}</h4>
         </div>
-        <div class="container-fluid">
+        <div class="page-content-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h6 class="card-title text-dark m-0">
+                                    Detalle de Registros
+                                    <div class="float-right">
+                                        <button
+                                            @click.prevent="newTag"
+                                            class="btn btn-sm btn-success waves-effect waves-light m-l-10"
+                                        >
+                                            <i class="fa fa-plus"></i>
+                                            {{ $t("actions.new") }}
+                                        </button>
+                                    </div>
+                                </h6>
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <data-table
+                                        :columns="columns"
+                                        :ajax="ajax"
+                                        width="100%"
+                                        @draw="draw"
+                                        ref="datatable-tag"
+                                    ></data-table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+        <!-- vista de consulta-->
+        <b-modal ref="view-tag" size="lg" title="Tag">
+            <h4>{{ $t("nav.Label") }}</h4>
+            <p class="text-muted">{{ tag.Label }}</p>
+            <template #modal-footer>
+                <a @click.prevent="editTag" class="btn btn-warning">
+                    <i class="fa fa-edit"></i>
+                    {{ $t("actions.edit") }}
+                </a>
+                <a
+                    @click.prevent="deleteTag"
+                    class="btn btn-danger"
+                >
+                    <i class="fa fa-trash"></i>
+                    {{ $t("actions.destroy") }}
+                </a>
+            </template>
+        </b-modal>
+        <!-- vista de consulta-->
+        <b-modal ref="frm-tag" size="lg" title="Tag">
+            <form role="form">
+                <div class="form-group">
+                    <label for="Label">Label</label>
+                    <input
+                        type="text"
+                        class="form-control"
+                        name="Label"
+                        v-model="tag.Label"
+                    />
+                    <ul
+                        class="parsley-errors-list filled"
+                        id="parsley-id-19"
+                        v-if="errorBag.Label"
+                    >
+                        <li class="parsley-required">{{ errorBag.Label }}</li>
+                    </ul>
+                </div>
+            </form>
+            <template #modal-footer>
+                <a
+                    @click.prevent="saveTag()"
+                    class="btn btn-success"
+                >
+                    <i class="fa fa-save"></i>
+                    {{ $t("actions.save") }}
+                </a>
+                <button
+                    type="button"
+                    class="btn btn-danger"
+                    @click.prevent="cancelTag()"
+                >
+                    <i class="fa fa-window-close"></i>
+                    {{ $t("actions.cancel") }}
+                </button>
+            </template>
+        </b-modal>
     </div>
 </template>
 
